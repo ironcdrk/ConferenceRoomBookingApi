6b8e9c4cb74900214c181ef9cbb3eba50c9dc0ce
Carlos Licto <carlosels.dev@gmail.com>
2026-01-02 18:53:10 +0000

    se añaden hasapitoken en la clase user y se linkea la ruta de register al controller Auth

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-    Route::post('register', ...);
+//Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
     Route::post('login', ...);
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', ...);
         Route::get('me', ...);
     });
-});
+//});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('rooms', ...);
